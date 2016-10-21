c73bf6d76d433a63848e65f57495da6037e33926
IIGhouLII <seifallah.jardak@gmail.com>
2016-10-21 16:30:14 +0300

    Read the name of the radiostation before playing it

--- Welcome.sh
+++ Welcome.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 amixer -c 0 set PCM 100%
 echo "Great to see you again"| festival --tts
-echo "my i p address, is,  `ifconfig eth0 |grep inet |grep netmask |cut -c14-26`"| festival --tts
+echo "my i p address, is,  `ifconfig eth0 | grep -w inet | tr -s ' ' | cut -d ' ' -f 3`"| festival --tts
 amixer -c 0 set PCM 80%
