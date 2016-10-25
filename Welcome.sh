aefcb2068e87ef5d1caa25e197c18b3dcd8244cf
IIGhouLII <seifallah.jardak@gmail.com>
2016-10-25 21:58:34 +0300

    Changed the script to work on Cmus instead of MOC

--- Welcome.sh
+++ Welcome.sh
@@ -2,4 +2,4 @@
 amixer -c 0 set PCM 100%
 echo "Great to see you again"| festival --tts
 echo "my i p address, is,  `ifconfig eth0 | grep -w inet | tr -s ' ' | cut -d ' ' -f 3`"| festival --tts
-amixer -c 0 set PCM 80%
+# amixer -c 0 set PCM 80%
